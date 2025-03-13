27cae22f35faed00e4f939ab24e4581bd6009f84
AdrianaCaballero1107 <manueladri188@gmail.com>
2025-03-12 18:18:21 -0600

    Actividad 7

---
+++ Java/scratch.java
@@ -0,0 +1,28 @@
+class Scratch {
+    public static void main(String[] args) {
+        int a= 38, b=17;
+        char letra ='A';
+        long numero = 11070308L;
+        float numeroflotante = 3.14f;
+        double numerodoble = 3.87113;
+        boolean valor = true;
+
+        int suma = a+ b;
+        int resta = a - b;
+        int multiplicacion = a * b;
+        int division = a / b;
+        int residuo = a % b;
+
+        System.out.println("Suma: "+ suma);
+        System.out.println("Resta: "+ resta);
+        System.out.println("Multiplicación: "+ multiplicacion);
+        System.out.println("División: " + division);
+        System.out.println("Residuo: "+ residuo);
+
+        if ( a > b) {
+            System.out.println("a es mayor que b");
+        }
+
+        }
+
+    }
